312418f00ccc661844d045edbeda2ece6623e086
Suhira30 <suhirabalarajan@gmail.com>
2026-02-20 13:21:37 +0530

    style: comments removed

--- src/api/profileApi.js
+++ src/api/profileApi.js
@@ -1,16 +1,10 @@
 import axiosInstance from './axios';
 
-/**
- * Get user profile
- */
 export const getUserProfile = async () => {
     const response = await axiosInstance.get('/profile/get/user-profile');
     return response.data;
 };
 
-/**
- * Get user timeline
- */
 export const getUserTimeline = async (sortBy = 'latest') => {
     const response = await axiosInstance.get('/profile/timeline', {
         params: { sortBy },
@@ -18,9 +12,6 @@ export const getUserTimeline = async (sortBy = 'latest') => {
     return response.data;
 };
 
-/**
- * Get filtered timeline
- */
 export const getFilteredTimeline = async (type = 'all') => {
     const response = await axiosInstance.get('/profile/timeline/filter', {
         params: { type },
